f6f51cabc80f8b7c16c9b9d5e375dafa36f56ef0
Udo.Bassey <Udo.Bassey@fintraksoftware.com>
2022-10-28 03:57:51 +0100

    Ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+ import Pic from './Aboya.jpg';
+ import vector from './logo.png';
+//  import text from './Footer_text.png';
+ import I4G from './I4G.png';
+//  import Git from './Icon.png';
+//  import Slack from './slack.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Header />
+        <WriteUps />
+        <Buttons />
+        {/* <Social /> */}
+        <Footer />
     </div>
   );
+}
+
+function Header(){
+  return(
+      <header>
+        <nav>
+          <img src= {Pic} id="profile__img" alt="profile Pic" width= "80px"/>
+         
+        </nav>
+      </header>
+  )
+}
+// function Social(){
+//   return(
+//       <div className='social'>
+//           <img src= {Git} id="git" alt="profile Pic" width= "80px"/>
+//           <img src= {Slack} id="slack__img" alt="profile Pic" width= "80px"/>
+//       </div>
+//   )
+// }
+
+function Footer(){
+  return(
+      <footer>
+        <hr></hr>
+        <div><img src= {vector}  alt="profile Pic" /></div>
+        {/* <div><img src= {text}  alt="profile Pic" /></div> */}
+        <span>HNG Internship 9 Frontend Task</span>
+        <div><img src= {I4G}  alt="profile Pic" /></div>
+        
+        
+        
+      </footer>
+  )
+}
+function WriteUps(){
+  return(
+    <div>
+    <h2 id='twitter'>Udo Bassey</h2>
+    <h2 id='slack' hidden>Aboya</h2>
+    </div>
+  )
+}
+function Buttons(){
+  return(
+        <div className='link-buttons'>
+           <a id='btn__zuri' href="https://training.zuri.team/" target="_blank" rel="noreferrer">
+          Zuri Team
+        </a>
+        <a id='books' href="http://books.zuri.team" target="_blank" rel="noreferrer">
+        Zuri Books
+        </a>
+        <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=UdoB_Bassey" target="_blank" rel="noreferrer">
+        Python Books
+        </a>
+        <a id='pitch' href="https://background.zuri.team" target="_blank" rel="noreferrer">
+        Background Check For Coders
+        </a>
+        <a id='book__design' href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer">
+        Design Books
+        </a>
+            
+        </div>
+  )
 }
 
 export default App;
